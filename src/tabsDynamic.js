15200cdb863f2f529e9c7884831dfaaf88a0af1e
Marques Woodson <marques.woodson@gmail.com>
2015-04-14 09:17:43 -0500

    Trying out the Ancestor component

--- src/tabsDynamic.js
+++ src/tabsDynamic.js
@@ -3,43 +3,39 @@ import {
     Template, 
     For, 
     bootstrap, 
-    Parent, 
+    Ancestor,
     DynamicComponent,
     PrivateComponentLoader,
     PrivateComponentLocation} from 'angular2/angular2';
 import MovieFactory from './services/movieFactory';
-
 
 @Component({
     selector: 'hello', 
     services: [MovieFactory]
 })
 @Template({
-    directives: [For, Tabs, Tab, DynamicComp],
+    directives: [For, Tabs, Tab],
     inline: `
       <tabs>
-        <dynamic-comp *for="#mov of tabs; #i=index" [component]="mov" [tab-title]="mov.title">{{mov.desc}}</dynamic-comp>
+        <tab *for="var mov of list" [tab-title]="mov.title">{{mov.desc}}</tab>
       </tabs>
     `,
 })
 export class Hello {
     constructor(movieFactory: MovieFactory) {
-        // this.factory = movieFactory;
-        // this.tabs = Tabs.tabs;
-        console.debug("tabs")
-        // this.list = [{title: 'first title', desc: 'first desc'}];
-        // this.factory.getMovies()
-        //     .then(mov => {
-        //         mov.movies.forEach(movie => {
-        //             this.list.push({
-        //                 title: movie.title, desc: movie.description
-        //             });
-        //             console.debug(this.list);
-        //         });
-        //     });
+        console.debug('doin it')
+        this.factory = movieFactory;
+        this.list = [];
+        this.factory.getMovies()
+            .then(mov => {
+                mov.movies.forEach(movie => {
+                    this.list.push({
+                        title: movie.title, desc: movie.description
+                    });
+                });
+            });
     }
 }
-
 
 @Component({
     selector: 'tabs'
@@ -59,13 +55,8 @@ export class Tabs {
     constructor() {
         this.tabs = [];
     }
-    
-    getTabs() {
-        return this.tabs;
-    }
 
     addTab(tab: Tab) {
-        console.debug('addTab', tab);
         if (this.tabs.length === 0) {
             tab.active = true
         }
@@ -80,35 +71,11 @@ export class Tabs {
     }
 }
 
-
-@DynamicComponent({
-    selector: 'dynamic-comp',
-    bind : {
-        component : 'component'
-    }
-})
-class DynamicComp {
-    tab:Tab;
-    constructor(loader:PrivateComponentLoader, location:PrivateComponentLocation) {
-        console.debug('loader')
-        this.loader = loader;
-        this.location = location;   
-    }
-    set component(ComponentConstructor) {
-        this.loader.load(ComponentConstructor, this.location).then((tab) => {
-            console.debug('IS there a tab', tab);
-            this.tab = tab;
-        });
-    }
-}
-
-
 @Component({
     selector: 'tab',
     bind: {
         'tabTitle': 'tab-title'
-    },
-    services: [MovieFactory]
+    }
 })
 @Template({
     inline: `
@@ -118,19 +85,8 @@ class DynamicComp {
     `
 })
 export class Tab {
-    constructor(@Parent() tabs: Tabs, movieFactory:MovieFactory) {
-        this.factory = movieFactory;
-        this.list = [{title: 'first title', desc: 'first desc'}];
-        this.factory.getMovies()
-            .then(mov => {
-                mov.movies.forEach(movie => {
-                    this.list.push({
-                        title: movie.title, desc: movie.description
-                    });
-                    console.debug('what', this.list);
-                    tabs.addTab(this.list);
-                });
-            });
+    constructor(@Ancestor() tabs: Tabs) {
+        tabs.addTab(this);
     }
 }
 export function main() {
